c9529a989a511873eb5be81e0b4c2bf4d8030b0c
Sarah <y.sarahc@gmail.com>
2016-10-01 13:45:29 +0100

    Fixes typo, adds POST request and initial database config

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
 
-app.listen(3000, function() {
-  console.log('listening on port 3000')
+app.use(bodyParser.urlencoded({extended: true}))
+
+MongoClient.connect('link-to-mongodb', function(err, database) {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
 })
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/quotes', function(req, res) {
-  console.log("testing foo and bar")
+app.post('/items', function(req, res) {
+  console.log(req.body)
 })
