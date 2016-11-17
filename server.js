840047fa15dd8424609683cfb02398692983d4bf
Sarah Young <sarah.young@Infras-MacBook-Pro.local>
2016-11-17 15:45:59 +0000

    Reverts tutorial logic to only desplay vanilla html; reverts tutorial html.

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 const express = require('express')
 const bodyParser= require('body-parser')
-const MongoClient = require('mongodb').MongoClient
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-var db
-
-MongoClient.connect('mongodb://<admin>:<admin>@ds047166.mlab.com:47166/personal-website', 
-function(err, database) {
-  if (err) return console.log(err)
-  db = database
-  app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+app.listen(3000, function() {
+  console.log('listening on 3000')
 })
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/items', function(req, res) {
-  console.log(req.body)
 })
